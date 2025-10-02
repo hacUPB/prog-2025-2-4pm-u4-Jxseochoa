9cdc93d8164269736d3af1099b51bef2df71bd59
Jose Ochoa <jose.ochoac@upb.edu.co>
2025-10-02 16:07:21 -0500

    Ejercicio 3 de la actividad 2 de listas.

--- listas/ejercicio2_3.py
+++ listas/ejercicio2_3.py
@@ -4,7 +4,7 @@
 
 def analizar_trayectoria(rumbo_planeado, rumbo_real, umbral_desviacion=5):
     desviaciones = []
-    i = 0
+    i = 1
     for rp, rr in zip(rumbo_planeado, rumbo_real):
         diferencia = rr - rp
         if diferencia < 0:
@@ -18,5 +18,5 @@ def analizar_trayectoria(rumbo_planeado, rumbo_real, umbral_desviacion=5):
 planeado = [45, 45, 45, 90, 90, 90, 180, 180, 225, 225, 270]
 real     = [43, 47, 48, 86, 91, 95, 183, 176, 222, 230, 265]
 
-desviaciones = analizar_trayectoria(planeado, real, 5)
-print("Desviaciones significativas en los puntos:", desviaciones)
+print(analizar_trayectoria(planeado, real, 5))
+
